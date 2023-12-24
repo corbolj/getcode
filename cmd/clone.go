c99fef699fcd86d62a771006bf17c2d973aa91e2
lcorbo-sada <louie.corbo@sada.com>
2023-12-23 20:06:57 -0500

    Adding first command, still need to implement cloning.

--- cmd/clone.go
+++ cmd/clone.go
@@ -1,1 +1,21 @@
 package getcode
+
+import (
+	getcode "github.com/corbolj/getcode/pkg"
+	"github.com/spf13/cobra"
+)
+
+var cloneCmd = &cobra.Command{
+	Use:   "clone",
+	Short: "clones down all repos in an organization or user",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		i := args[0]
+		getcode.Clone(i)
+	},
+}
+
+func init() {
+	// Pull configs into object
+	rootCmd.AddCommand(cloneCmd)
+}
---
+++ pkg/clone.go
@@ -0,0 +1,17 @@
+package getcode
+
+import "fmt"
+
+func Clone(url string) {
+	fmt.Println("Still need to implement.")
+}
+
+func cloneRepos(url string) {
+	fmt.Println("work in progress")
+
+	// Get Organization (url) return org
+
+	// Get list of repos (org) return repos[]
+
+	// Clone all repos[]
+}
